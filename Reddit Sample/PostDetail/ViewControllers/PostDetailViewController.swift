fac84acdddabc535694ea5aad337fc2eb3974a71
Kevin Belter <kevin.belter@gmail.com>
2018-07-01 12:28:43 -0500

    Creating view controllers class and match them in the storyboard

---
+++ Reddit Sample/PostDetail/ViewControllers/PostDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PostDetailViewController.swift
+//  Reddit Sample
+//
+//  Created by Kevin on 7/1/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+import UIKit
+
+final class PostDetailViewController: UIViewController {
+    
+}
---
+++ Reddit Sample/PostList/ViewControllers/PostListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PostListViewController.swift
+//  Reddit Sample
+//
+//  Created by Kevin on 7/1/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+import UIKit
+
+final class PostListViewController: UIViewController {
+    
+}
